9cddb4c8526f1035a67ff5802aa3207427bd5b79
imide <43657624+imide@users.noreply.github.com>
2024-04-04 13:20:13 -0600

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,41 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import remarkMdxCodeMeta from "remark-mdx-code-meta";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import react from "@astrojs/react";
+import bun from "astro-bun-adapter";
+import unocss from "@unocss/astro";
+import swup from "@swup/astro";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://imide.is-a.dev",
+  integrations: [
+    tailwind(),
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+        theme: "css-variables",
+      },
+      remarkPlugins: [remarkMdxCodeMeta, remarkMath],
+      rehypePlugins: [rehypeKatex],
+      remarkRehype: {
+        footnoteLabel: "Footnotes",
+      },
+      gfm: false,
+    }),
+    react(),
+    unocss(),
+    swup({
+      smoothScrolling: true,
+    }),
+  ],
+  vite: {
+    ssr: {
+      noExternal: ["mafs"],
+    },
+  },
+  adapter: bun(),
+});
