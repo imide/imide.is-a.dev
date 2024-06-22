c0d12fc27162b6cec219843da9fa6808973e98fd
imide <43657624+imide@users.noreply.github.com>
2024-06-22 12:59:21 -0600

    shit fuck FUCK

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,31 @@
-import {defineConfig} from "astro/config";
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import remarkMdxCodeMeta from "remark-mdx-code-meta";
 import react from "@astrojs/react";
 import unocss from "@unocss/astro";
 import swup from "@swup/astro";
-import cloudflare from "@astrojs/cloudflare";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://imide.is-a.dev",
-    integrations: [tailwind(), mdx({
-        syntaxHighlight: "shiki",
-        shikiConfig: {
-            theme: "css-variables"
-        },
-        remarkPlugins: [remarkMdxCodeMeta],
-        remarkRehype: {
-            footnoteLabel: "Footnotes"
-        },
-        gfm: false
-    }), react(), unocss(), swup({
-        smoothScrolling: true
-    })],
-    adapter: cloudflare({
-        imageService: 'cloudflare',
-    }),
-    output: "server"
+  site: "https://imide.is-a.dev",
+  integrations: [tailwind(), mdx({
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "css-variables"
+    },
+    remarkPlugins: [remarkMdxCodeMeta],
+    remarkRehype: {
+      footnoteLabel: "Footnotes"
+    },
+    gfm: false
+  }), react(), unocss(), swup({
+    smoothScrolling: true
+  })],
+  adapter: netlify({
+    edgeMiddleware: true
+  }
+  ),
+  output: "server"
 });
